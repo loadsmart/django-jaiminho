1efe2647c7995b08838fefa85b84c517106ccb58
Rafael Zanetti <rafael.zanetti@loadsmart.com>
2022-06-07 19:58:09 -0300

    Adding command to clean old events

---
+++ jaiminho/tests/management/test_event_cleaner.py
@@ -0,0 +1,74 @@
+import pytest
+
+from datetime import timedelta
+from django.utils import timezone
+
+from jaiminho.management import EventCleanerCommand
+from jaiminho.models import Event
+from jaiminho.tests.factories import EventFactory
+
+pytestmark = pytest.mark.django_db
+
+
+class TestEventCleanerCommand:
+
+    TIME_TO_DELETE = timedelta(days=5)
+
+    @pytest.fixture
+    def older_events(self):
+        return EventFactory.create_batch(
+            2, sent_at=timezone.now() - self.TIME_TO_DELETE - timedelta(days=1)
+        )
+
+    @pytest.fixture
+    def newer_events(self):
+        return EventFactory.create_batch(
+            2, sent_at=timezone.now() - self.TIME_TO_DELETE + timedelta(days=1)
+        )
+
+    @pytest.fixture
+    def not_sent_events(self):
+        return EventFactory.create_batch(2, sent_at=None)
+
+    def test_command_without_time_to_delete_configuration_raises_error_and_doesnt_delete(
+        self, older_events, newer_events, not_sent_events
+    ):
+        assert len(Event.objects.all()) == 6
+
+        with pytest.raises(AssertionError):
+            EventCleanerCommand().handle()
+
+        assert len(Event.objects.all()) == 6
+
+    def test_command_with_misconfigured_time_to_delete_raises_error_and_doesnt_delete(
+        self, mocker, older_events, newer_events, not_sent_events
+    ):
+        mocker.patch("jaiminho.send.settings.time_to_delete", "not-a-timedelta")
+
+        assert len(Event.objects.all()) == 6
+
+        with pytest.raises(AssertionError):
+            EventCleanerCommand().handle()
+
+        assert len(Event.objects.all()) == 6
+
+    def test_command_deletes_older_events(
+        self, mocker, older_events, newer_events, not_sent_events
+    ):
+        mocker.patch("jaiminho.send.settings.time_to_delete", self.TIME_TO_DELETE)
+
+        assert len(Event.objects.all()) == 6
+        EventCleanerCommand().handle()
+
+        remaining_events = Event.objects.all()
+        assert len(remaining_events) == 4
+        assert set(remaining_events) == set([*newer_events, *not_sent_events])
+
+    def test_command_doesnt_delete_when_there_are_no_older_events(
+        self, mocker, newer_events, not_sent_events
+    ):
+        mocker.patch("jaiminho.send.settings.time_to_delete", self.TIME_TO_DELETE)
+
+        assert len(Event.objects.all()) == 4
+        EventCleanerCommand().handle()
+        assert len(Event.objects.all()) == 4
