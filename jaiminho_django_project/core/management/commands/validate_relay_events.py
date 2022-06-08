2019f4df71f57b6f9cee7a77afb5c97d2b617aa0
yanhenning <yan.henning@loadsmart.com>
2022-06-08 10:03:23 -0300

    adjust the capture_exception_fn inside relay_events

--- jaiminho_django_project/core/management/commands/validate_relay_events.py
+++ jaiminho_django_project/core/management/commands/validate_relay_events.py
@@ -1,9 +1,8 @@
 import sentry_sdk
 
 from jaiminho.management.relay_events import RelayEventsCommand
-from jaiminho_django_project.send import notify
 
 
 class Command(RelayEventsCommand):
-    notify_fn = notify
-    capture_exception_fn = sentry_sdk.capture_exception
+    def __init__(self):
+        super(Command, self).__init__()
