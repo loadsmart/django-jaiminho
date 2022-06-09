19e0a92ff831252a88feaa6ce135c9347cde1769
Gustavo Henrique Rodrigues Pinto Tomas <gustavo.rodrigues@loadsmart.com>
2022-06-09 12:14:36 -0300

    WGO-20: Conditional import for JSONField to support old Django versions

--- jaiminho/models.py
+++ jaiminho/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+try:
+    from django.db.models import JSONField
+except:
+    from django.contrib.postgres.fields import JSONField
+
 
 class Event(models.Model):
     type = models.CharField(max_length=64)
     action = models.CharField(max_length=64)
-    payload = models.JSONField()
+    payload = JSONField()
     encoder = models.CharField(max_length=255, null=True)
     function_signature = models.CharField(max_length=255, null=True)
     options = models.TextField(blank=True)
