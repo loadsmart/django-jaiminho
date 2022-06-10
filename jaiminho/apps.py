2fc6902c69d79b4dda8f378b274ce238ec4f2308
Gustavo Henrique Rodrigues Pinto Tomas <gustavo.rodrigues@loadsmart.com>
2022-06-10 09:08:00 -0300

    Improvements

--- jaiminho/apps.py
+++ jaiminho/apps.py
@@ -3,7 +3,7 @@ from django.utils import version
 
 
 class JaiminhoConfig(AppConfig):
-    if version.get_version() > '3.2':
+    if version.get_version() >= '3.2':
         default_auto_field = "django.db.models.BigAutoField"
     else:
         default_auto_field = "django.db.models.AutoField"
