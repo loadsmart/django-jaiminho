2163a64a91b489bca65abf940e8d74b4067233d5
Leo Antunes <antunesleo4@gmail.com>
2022-07-08 09:40:13 -0300

    add option to not raise exception when save_to_outbox failed to publish event

--- jaiminho/settings.py
+++ jaiminho/settings.py
@@ -11,6 +11,8 @@ except AttributeError:
 persist_all_events = jaiminho_settings.get("PERSIST_ALL_EVENTS", False)
 default_encoder = jaiminho_settings.get("DEFAULT_ENCODER", DjangoJSONEncoder)
 time_to_delete = jaiminho_settings.get("TIME_TO_DELETE", timedelta(days=7))
+raise_on_publishing_error = jaiminho_settings.get("RAISE_ON_PUBLISHING_ERROR", True)
+
 default_capture_exception = jaiminho_settings.get(
     "DEFAULT_CAPTURE_EXCEPTION", sentry_sdk.capture_exception
 )
