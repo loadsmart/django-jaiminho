d79763a5176dfc0a323795957ff74e010a578d36
Leo Antunes <antunesleo4@gmail.com>
2022-07-19 09:51:55 -0300

    set delete_after_send default as false

--- jaiminho/settings.py
+++ jaiminho/settings.py
@@ -11,7 +11,7 @@ except AttributeError:
 persist_all_events = jaiminho_settings.get("PERSIST_ALL_EVENTS", False)
 default_encoder = jaiminho_settings.get("DEFAULT_ENCODER", DjangoJSONEncoder)
 time_to_delete = jaiminho_settings.get("TIME_TO_DELETE", timedelta(days=7))
-delete_after_send = jaiminho_settings.get("DELETE_AFTER_SEND", True)
+delete_after_send = jaiminho_settings.get("DELETE_AFTER_SEND", False)
 
 default_capture_exception = jaiminho_settings.get(
     "DEFAULT_CAPTURE_EXCEPTION", sentry_sdk.capture_exception
