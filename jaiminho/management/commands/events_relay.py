21a9d16876dfb1513d9fba54899c0d454d2a2feb
yanhenning <yan.henning@loadsmart.com>
2022-07-26 15:57:13 -0300

    Add support to unpickle objects

--- jaiminho/management/commands/events_relay.py
+++ jaiminho/management/commands/events_relay.py
@@ -1,11 +1,9 @@
 import logging
+import dill as pickle
 
 from django.core.management import BaseCommand
-from django.utils import timezone
 
 from jaiminho import settings
-from jaiminho.func_handler import load_func_from_path
-from jaiminho.kwargs_handler import load_kwargs
 from jaiminho.models import Event
 from jaiminho.signals import (
     event_published_by_events_relay,
@@ -26,27 +24,26 @@ class Command(BaseCommand):
             return
 
         for event in failed_events:
+            message = pickle.loads(event.message)
+            kwargs = pickle.loads(event.kwargs)
             try:
                 original_fn = self._extract_original_func(event)
-                encoder = load_func_from_path(event.encoder)
-                original_fn(
-                    event.payload, encoder=encoder, **load_kwargs(event.options)
-                )
+                original_fn(message, **kwargs)
                 log.info(f"JAIMINHO-EVENTS-RELAY: Event sent. Event {event}")
 
                 if settings.delete_after_send:
                     event.delete()
                     log.info(
-                        f"JAIMINHO-EVENTS-RELAY: Event deleted after success send. Event: {event}, Payload: {event.payload}"
+                        f"JAIMINHO-EVENTS-RELAY: Event deleted after success send. Event: {event}, Payload: {message}"
                     )
                 else:
                     event.mark_as_sent()
                     log.info(
-                        f"JAIMINHO-EVENTS-RELAY: Event marked as sent. Event: {event}, Payload: {event.payload}"
+                        f"JAIMINHO-EVENTS-RELAY: Event marked as sent. Event: {event}, Payload: {message}"
                     )
 
                 event_published_by_events_relay.send(
-                    sender=original_fn, event_payload=event.payload
+                    sender=original_fn, event_payload=message
                 )
 
             except (ModuleNotFoundError, AttributeError) as e:
@@ -57,7 +54,7 @@ class Command(BaseCommand):
                 log.warning(f"JAIMINHO-EVENTS-RELAY: An error occurred when relaying event: {event} | Error: {str(e)}")
                 original_fn = self._extract_original_func(event)
                 event_failed_to_publish_by_events_relay.send(
-                    sender=original_fn, event_payload=event.payload
+                    sender=original_fn, event_payload=message
                 )
                 self._capture_exception(e)
 
@@ -67,6 +64,6 @@ class Command(BaseCommand):
             capture_exception(exception)
 
     def _extract_original_func(self, event):
-        fn = load_func_from_path(event.function_signature)
+        fn = pickle.loads(event.function)
         original_fn = getattr(fn, "original_func", fn)
         return original_fn
