c50b1e3297d7449062668d791f3a275f3025084f
Leo Antunes <antunesleo4@gmail.com>
2022-09-02 10:13:15 -0300

    allow overwrite publish strategy on save to outbox with stream decorator

--- jaiminho/send.py
+++ jaiminho/send.py
@@ -18,11 +18,12 @@ def save_to_outbox(func):
     return inner
 
 
-def save_to_outbox_stream(stream):
+def save_to_outbox_stream(stream, overwrite_strategy_with=None):
     def decorator(func):
         @wraps(func)
         def inner(payload, **kwargs):
-            publish_strategy = create_publish_strategy(settings.publish_strategy)
+            _publish_strategy = overwrite_strategy_with if overwrite_strategy_with else settings.publish_strategy
+            publish_strategy = create_publish_strategy(_publish_strategy)
             publish_strategy.publish(payload, kwargs, func, stream)
 
         inner.original_func = func
