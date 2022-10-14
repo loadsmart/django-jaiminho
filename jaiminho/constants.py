095a44991f3297d86231b55c50f83f5c6e1268c0
Leo Antunes <antunesleo4@gmail.com>
2022-10-14 08:22:17 -0300

    add strategy column

--- jaiminho/constants.py
+++ jaiminho/constants.py
@@ -1,3 +1,9 @@
 class PublishStrategyType:
     PUBLISH_ON_COMMIT = "publish-on-commit"
     KEEP_ORDER = "keep-order"
+
+    CHOICES = (
+        (PUBLISH_ON_COMMIT, "Publish on Commit"),
+        (KEEP_ORDER, "Keep Order")
+    )
+
