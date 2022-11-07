92164d7862e51a72c89160b6a7fa407d07df3d23
yanhenning <yan.henning@loadsmart.com>
2022-11-07 17:41:55 -0300

    send kwargs to signals

--- jaiminho_django_project/app/signals.py
+++ jaiminho_django_project/app/signals.py
@@ -4,25 +4,25 @@ from django.dispatch import receiver
 import jaiminho.signals
 
 
-def log_metric(name, event_payload):
+def log_metric(name, event_payload, **kwargs):
     logging.info("%s: %s", name, event_payload)
 
 
 @receiver(jaiminho.signals.event_published)
-def on_event_published(sender, event_payload, **kwargs):
-    log_metric("event-published", event_payload)
+def on_event_published(signal, sender, event_payload, **kwargs):
+    log_metric("event-published", event_payload, **kwargs)
 
 
 @receiver(jaiminho.signals.event_failed_to_publish)
-def on_event_not_published(sender, event_payload, **kwargs):
-    log_metric("event-failed-to-publish", event_payload)
+def on_event_not_published(signal, sender, event_payload, **kwargs):
+    log_metric("event-failed-to-publish", event_payload, **kwargs)
 
 
 @receiver(jaiminho.signals.event_published_by_events_relay)
-def on_event_published_through_relay_command(sender, event_payload, **kwargs):
-    log_metric("event-published-through-outbox", event_payload)
+def on_event_published_through_relay_command(signal, sender, event_payload, **kwargs):
+    log_metric("event-published-through-outbox", event_payload, **kwargs)
 
 
 @receiver(jaiminho.signals.event_failed_to_publish_by_events_relay)
-def on_event_not_published_through_relay_command(sender, event_payload, **kwargs):
-    log_metric("event-failed-to-publish-through-outbox", event_payload)
+def on_event_not_published_through_relay_command(signal, sender, event_payload, **kwargs):
+    log_metric("event-failed-to-publish-through-outbox", event_payload, **kwargs)
