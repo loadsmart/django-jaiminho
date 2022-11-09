a98d7d34c1421410449f55866d098c76c53df775
yanhenning <yan.henning@loadsmart.com>
2022-11-09 19:23:36 -0300

    make tests more clear

--- jaiminho_django_project/tests/management/commands/test_validate_relay_events.py
+++ jaiminho_django_project/tests/management/commands/test_validate_relay_events.py
@@ -216,30 +216,23 @@ class TestValidateEventsRelay:
         with pytest.raises(Exception):
             command = validate_events_relay.Command()
             command.event_relayer = event_relayer_mock
-            call_command(command, "--no-run-in-loop")
+            call_command(command, "--run-in-loop", "--loop-interval", 0.1)
 
         assert event_relayer_mock.relay.call_count == 3
 
     def test_accept_no_run_in_loop_as_argument(
         self,
         mock_log_metric,
-        failed_event,
-        mock_internal_notify,
-        mock_should_not_delete_after_send,
         mocker,
     ):
-        mocker.patch("jaiminho.settings.publish_strategy", PublishStrategyType.PUBLISH_ON_COMMIT)
+        event_relayer_mock = mocker.MagicMock(spec=EventRelayer)
+        event_relayer_mock.relay.side_effect = [None, None, Exception()]
 
-        assert Event.objects.all().count() == 1
+        command = validate_events_relay.Command()
+        command.event_relayer = event_relayer_mock
+        call_command(command, "--no-run-in-loop")
 
-        with freeze_time("2022-10-31"):
-            call_command(validate_events_relay.Command(), "--no-run-in-loop")
-
-        mock_internal_notify.assert_called_once()
-        mock_internal_notify.assert_called_with(dill.loads(failed_event.message))
-        assert Event.objects.all().count() == 1
-        event = Event.objects.all()[0]
-        assert event.sent_at == datetime(2022, 10, 31, tzinfo=UTC)
+        assert event_relayer_mock.relay.call_count == 1
 
     @pytest.mark.parametrize(
         "publish_strategy",
