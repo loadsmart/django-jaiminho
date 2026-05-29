5d0b21a972bce8f846c05a7e6f25851cfb49e99f
Lucas de Almeida Tonelli <tonellicontato@gmail.com>
2026-05-29 10:20:15 -0300

    feat: add events admin

--- jaiminho_django_test_project/settings.py
+++ jaiminho_django_test_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django.contrib.sessions",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
